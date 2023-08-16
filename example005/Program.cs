ff5be8f166825a029557f28e5d7bb053b324e448
roblox <wqweww22@gmail.com>
2023-08-16 14:04:24 +0300

    commit5

---
+++ example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")   
+
+      Console.WriteLine("шлюха!");
+else
+{
+    Console.Write("привет, ");
+    Console.WriteLine(username);
+}
